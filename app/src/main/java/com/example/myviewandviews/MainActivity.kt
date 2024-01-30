738c843d81a796158d64a27bc4cad336248d639d
WINDOWS 10 <chesarilandi@gmail.com>
2024-01-30 11:42:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myviewandviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.viewsgroup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.myviewandviews.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
